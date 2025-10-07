8eb90bdaaa7e30cd8da7bb0d60d991e8b76c0b5d
knight <941122922@qq.com>
2025-10-07 16:23:49 +0800

    fix: 修复web显示情况下容器宽度取自窗口宽度

--- lib/y_month_Item.dart
+++ lib/y_month_Item.dart
@@ -38,153 +38,167 @@ class YMonthItem extends StatelessWidget {
 
     int lastDay = DateTime(currentYear, currentMonth + 1, 0).day;
     int emptyDays = DateTime(currentYear, currentMonth, 1).weekday % 7;
-    double dayItemWidth = (MediaQuery.of(context).size.width ~/ 7).toDouble();
 
-    return IntrinsicHeight(
-      child: Stack(
-        children: <Widget>[
-          // 背景
-          Align(
-            child: Text(
-              "$currentMonth",
-              style: TextStyle(
-                fontSize: 150.0,
-                fontWeight: FontWeight.w500,
-                color: color.withAlpha(10),
-              ),
-            ),
-          ),
-          Column(
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return IntrinsicHeight(
+          child: Stack(
             children: <Widget>[
-              YMonthTitle(year: currentYear, month: currentMonth),
-              Wrap(
-                children: List.generate(lastDay + emptyDays, (i) {
-                  int day = i - emptyDays + 1;
+              // 背景
+              Align(
+                child: Text(
+                  "$currentMonth",
+                  style: TextStyle(
+                    fontSize: 150.0,
+                    fontWeight: FontWeight.w500,
+                    color: color.withAlpha(10),
+                  ),
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  YMonthTitle(year: currentYear, month: currentMonth),
+                  Wrap(
+                    children: List.generate(lastDay + emptyDays, (i) {
+                      int day = i - emptyDays + 1;
 
-                  DateTime currentDate = DateTime(
-                    currentYear,
-                    currentMonth,
-                    day,
-                  );
-                  bool isEmpty = day < 1;
-                  bool disable =
-                      currentDate.isBefore(DateUtils.dateOnly(minDate)) ||
-                      currentDate.isAfter(DateUtils.dateOnly(maxDate));
+                      DateTime currentDate = DateTime(
+                        currentYear,
+                        currentMonth,
+                        day,
+                      );
+                      bool isEmpty = day < 1;
+                      bool disable =
+                          currentDate.isBefore(DateUtils.dateOnly(minDate)) ||
+                          currentDate.isAfter(DateUtils.dateOnly(maxDate));
 
-                  Status status = Status(
-                    "",
-                    null,
-                    const Color(0xff323233),
-                    null,
-                  );
-                  if (isRange) {
-                    if (selectDate.length == 2 &&
-                        DateUtils.isSameDay(
-                          selectDate.first,
-                          selectDate.last,
-                        ) &&
-                        DateUtils.isSameDay(currentDate, selectDate.first)) {
-                      status = Status(
-                        "开始/结束",
-                        color,
-                        Colors.white,
-                        BorderRadius.circular(4.0),
-                      );
-                    } else if (selectDate.isNotEmpty &&
-                        DateUtils.isSameDay(currentDate, selectDate.first)) {
-                      status = Status(
-                        "开始",
-                        color,
-                        Colors.white,
-                        BorderRadius.horizontal(left: Radius.circular(4.0)),
-                      );
-                    } else if (selectDate.length == 2 &&
-                        !isEmpty &&
-                        currentDate.isAfter(selectDate.first!) &&
-                        currentDate.isBefore(selectDate.last!)) {
-                      status = Status(
+                      Status status = Status(
                         "",
-                        color.withAlpha((255 * 0.1).round()),
                         null,
+                        const Color(0xff323233),
                         null,
                       );
-                    } else if (selectDate.length == 2 &&
-                        DateUtils.isSameDay(currentDate, selectDate.last)) {
-                      status = Status(
-                        "结束",
-                        color,
-                        Colors.white,
-                        BorderRadius.horizontal(right: Radius.circular(4.0)),
-                      );
-                    }
-                  } else {
-                    if (!isEmpty &&
-                        selectDate.isNotEmpty &&
-                        DateUtils.isSameDay(currentDate, selectDate.first)) {
-                      status = Status(
-                        "",
-                        color,
-                        Colors.white,
-                        BorderRadius.circular(4.0),
-                      );
-                    }
-                  }
+                      if (isRange) {
+                        if (selectDate.length == 2 &&
+                            DateUtils.isSameDay(
+                              selectDate.first,
+                              selectDate.last,
+                            ) &&
+                            DateUtils.isSameDay(
+                              currentDate,
+                              selectDate.first,
+                            )) {
+                          status = Status(
+                            "开始/结束",
+                            color,
+                            Colors.white,
+                            BorderRadius.circular(4.0),
+                          );
+                        } else if (selectDate.isNotEmpty &&
+                            DateUtils.isSameDay(
+                              currentDate,
+                              selectDate.first,
+                            )) {
+                          status = Status(
+                            "开始",
+                            color,
+                            Colors.white,
+                            BorderRadius.horizontal(left: Radius.circular(4.0)),
+                          );
+                        } else if (selectDate.length == 2 &&
+                            !isEmpty &&
+                            currentDate.isAfter(selectDate.first!) &&
+                            currentDate.isBefore(selectDate.last!)) {
+                          status = Status(
+                            "",
+                            color.withAlpha((255 * 0.1).round()),
+                            null,
+                            null,
+                          );
+                        } else if (selectDate.length == 2 &&
+                            DateUtils.isSameDay(currentDate, selectDate.last)) {
+                          status = Status(
+                            "结束",
+                            color,
+                            Colors.white,
+                            BorderRadius.horizontal(
+                              right: Radius.circular(4.0),
+                            ),
+                          );
+                        }
+                      } else {
+                        if (!isEmpty &&
+                            selectDate.isNotEmpty &&
+                            DateUtils.isSameDay(
+                              currentDate,
+                              selectDate.first,
+                            )) {
+                          status = Status(
+                            "",
+                            color,
+                            Colors.white,
+                            BorderRadius.circular(4.0),
+                          );
+                        }
+                      }
 
-                  return SizedBox(
-                    width: dayItemWidth,
-                    height: 60.0,
-                    child: Visibility(
-                      visible: !isEmpty,
-                      child: Material(
-                        type: MaterialType.transparency,
-                        child: Ink(
-                          decoration: BoxDecoration(
-                            borderRadius: status.radius,
-                            color: status.bgColor,
-                          ),
-                          child: InkWell(
-                            borderRadius: BorderRadius.circular(4.0),
-                            onTap:
-                                isEmpty || disable
-                                    ? null
-                                    : () => onSelect?.call(currentDate),
-                            child: Stack(
-                              children: <Widget>[
-                                Center(
-                                  child: Text(
-                                    "$day",
-                                    style: TextStyle(
-                                      fontSize: 16.0,
-                                      color:
-                                          disable
-                                              ? const Color(0xffc8c9cc)
-                                              : status.textColor,
+                      return SizedBox(
+                        width: (constraints.maxWidth ~/ 7).toDouble(),
+                        height: 60.0,
+                        child: Visibility(
+                          visible: !isEmpty,
+                          child: Material(
+                            type: MaterialType.transparency,
+                            child: Ink(
+                              decoration: BoxDecoration(
+                                borderRadius: status.radius,
+                                color: status.bgColor,
+                              ),
+                              child: InkWell(
+                                borderRadius: BorderRadius.circular(4.0),
+                                onTap:
+                                    isEmpty || disable
+                                        ? null
+                                        : () => onSelect?.call(currentDate),
+                                child: Stack(
+                                  children: <Widget>[
+                                    Center(
+                                      child: Text(
+                                        "$day",
+                                        style: TextStyle(
+                                          fontSize: 16.0,
+                                          color:
+                                              disable
+                                                  ? const Color(0xffc8c9cc)
+                                                  : status.textColor,
+                                        ),
+                                      ),
                                     ),
-                                  ),
-                                ),
-                                Align(
-                                  alignment: const Alignment(0, 0.8),
-                                  child: Text(
-                                    status.text,
-                                    style: const TextStyle(
-                                      fontSize: 10.0,
-                                      color: Colors.white,
+                                    Align(
+                                      alignment: const Alignment(0, 0.8),
+                                      child: Text(
+                                        status.text,
+                                        style: const TextStyle(
+                                          fontSize: 10.0,
+                                          color: Colors.white,
+                                        ),
+                                      ),
                                     ),
-                                  ),
+                                  ],
                                 ),
-                              ],
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ),
-                  );
-                }),
+                      );
+                    }),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
